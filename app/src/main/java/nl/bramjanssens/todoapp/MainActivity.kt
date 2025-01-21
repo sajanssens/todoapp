998cf9c8c8cecf6afeda433eaf6812d08ffcb3e7
Bram Janssens <s.a.janssens@gmail.com>
2025-01-21 11:58:41 +0100

    Checkbox working and textinput added.

--- app/src/main/java/nl/bramjanssens/todoapp/MainActivity.kt
+++ app/src/main/java/nl/bramjanssens/todoapp/MainActivity.kt
@@ -3,12 +3,7 @@ package nl.bramjanssens.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import nl.bramjanssens.todoapp.manageitems.TodoItemLazyList
-import nl.bramjanssens.todoapp.manageitems.todos
+import nl.bramjanssens.todoapp.manageitems.TodoMain
 import nl.bramjanssens.todoapp.ui.theme.TodoAppTheme
 
 class MainActivity :
@@ -22,35 +17,8 @@ class MainActivity :
         setContent { // verplicht
             // hier: welke composables (of oude onderdelen) worden getoond
             TodoAppTheme { // modifier verandert styling, behaviour, ...; is een decorator; kun je chainen en worden doorgegeven naar de children
-                //            bijv. als je op toplevel dark mode meegeeft, wordt alles dark mode
-//                Greeting(
-//                    name = "Bram"
-//                )
-//                val i = TodoItem("Training volgen")
-//                ItemRow(item = i, onComplete = {
-//                    i.toggle()
-//                })
-
-//                DemoDetectDragGestures()
-//                TodoItemsList(todos())
-                TodoItemLazyList(todos())
+                TodoMain()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/nl/bramjanssens/todoapp/manageitems/ManageItems.kt
+++ app/src/main/java/nl/bramjanssens/todoapp/manageitems/ManageItems.kt
@@ -2,26 +2,38 @@ package nl.bramjanssens.todoapp.manageitems
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import nl.bramjanssens.todoapp.ui.theme.TodoAppTheme
 
 @Composable
-fun TodoItemRow(todo: TodoItem, modifier: Modifier = Modifier, onComplete: () -> Unit) {
+fun TodoItemRow(todo: TodoItem, modifier: Modifier = Modifier, completeTodoItem: () -> Unit) {
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Checkbox(todo.completed, onCheckedChange = { onComplete() })
+        val checked = remember { mutableStateOf(todo.completed) }
+        Checkbox(checked = checked.value, onCheckedChange = {
+            completeTodoItem()
+            checked.value = it
+        })
         Text(text = todo.title)
     }
 }
@@ -31,7 +43,7 @@ fun TodoItemList(todos: List<TodoItem>, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
+            .verticalScroll(rememberScrollState()) // remember the state; rememberSaveable keeps it even after the whole activity was destroyed and reloaded
     ) {
         todos.forEach { // will all get rendered eagerly, even when not necessary (yet)
             TodoItemRow(it) { it.toggle() }
@@ -51,12 +63,69 @@ fun TodoItemLazyList(todos: List<TodoItem>, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun TodoItemInput(modifier: Modifier = Modifier, onOk: (String) -> Unit) {
+    val textValue = remember { mutableStateOf("") }
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        TextField(
+            value = textValue.value,
+            onValueChange = {
+                textValue.value = it
+            },
+            label = {
+                Text(text = "Add item")
+            },
+            placeholder = {
+                Text(text = "title...")
+            },
+        )
+        Button(
+            modifier = modifier,
+            onClick = {
+                onOk(textValue.value)
+                textValue.value = ""
+            }
+        ) {
+            Text("Ok")
+        }
+    }
+}
+
+
+@Composable
+fun TodoMain(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        val todos = remember { mutableStateListOf(*todos().toTypedArray()) }
+        TodoItemInput { todos.add(TodoItem(it)) }
+        TodoItemLazyList(todos)
+    }
+}
+
+@Composable
+fun ToggleableCheckbox() {
+    // Remember the state of the checkbox
+    val isChecked = remember { mutableStateOf(false) }
+
+    // Checkbox that toggles its state when clicked
+    Checkbox(
+        checked = isChecked.value,
+        onCheckedChange = { isChecked.value = it }
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun ShowPreview() {
     TodoAppTheme {
-        TodoItemList(todos())
+        TodoMain()
     }
 }
 
-fun todos(): List<TodoItem> = List(25) { TodoItem("Item ${it + 1}") }
+fun todos(): List<TodoItem> = List(15) { TodoItem("Item ${it + 1}") }
